5629a633cf737db0902f23a891c993c5d25c7815
Jesse Portnoy <jesse@packman.io>
2023-08-04 00:48:36 +0100

    shellcheck

---
+++ run-shellcheck.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#
+
+set -e -o pipefail
+MYCMD=shellcheck
+if ! command -v $MYCMD &> /dev/null ; then
+    echo "$MYCMD not installed or available in the PATH" >&2
+    exit 1
+fi
+
+for FILE in "$@"; do
+    set +e
+    if grep -q "^#\!.*sh" "$FILE";then
+    set -e
+	shellcheck "$FILE"
+    fi
+done
