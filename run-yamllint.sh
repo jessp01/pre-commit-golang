452602b85aa3a725bab7e6f5d84afaee0bd32be7
Jesse Portnoy <jesse@packman.io>
2023-11-09 18:11:04 +0000

    Add lint checks for XML and YAML
    Author:    Jesse Portnoy <jesse@packman.io>
    Date:      Thu Nov 9 18:11:04 2023 +0000

---
+++ run-yamllint.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#
+
+set -e -o pipefail
+MYCMD=yamllint
+if ! command -v $MYCMD &> /dev/null ; then
+    echo "$MYCMD not installed or available in the PATH" >&2
+    exit 1
+fi
+
+for FILE in "$@"; do
+    set +e
+    if file "$FILE"|grep -q "JSON" ;then
+    set -e
+	$MYCMD "$FILE"
+    fi
+done
